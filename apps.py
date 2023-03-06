d440713edceb151d8a0c8fc20e734abd19efa4aa
Darth Vader <danieltarancon@gmail.com>
2023-03-06 18:41:26 +0100

    Update apps.py

--- apps.py
+++ apps.py
@@ -19,14 +19,14 @@ internal_dir = "/Users/danielmulatarancon/Desktop/Documents/HACKING TIME/Brainne
 
 # Defining a function to download files from the FTP server
 def download_files():
-    # Connect to FTP server
+    # Connecting to the FTP server
     with ftplib.FTP(ftp_host, ftp_user, ftp_password) as ftp:
-        # List files in FTP directory
+        # Listing files in the FTP directory
         files = ftp.nlst()
-        # Check if local directory exists, if not create it
+        # Checking if the local directory exists, if not I create it
         if not os.path.exists(local_dir):
             os.makedirs(local_dir)
-        # Download each file to local directory
+        # Downloading each file to the local directory
         for file in files:
             local_file_path = os.path.join(local_dir, file)
             try:
@@ -37,10 +37,10 @@ def download_files():
                 print(f"Error downloading {file}: {e}")
             except Exception as e:
                 print(f"Unexpected error downloading {file}: {e}")
-        # Move files from local directory to internal network directory
+        # Moving files from the local directory to the internal network directory
         for file in os.listdir(local_dir):
             shutil.move(os.path.join(local_dir, file), os.path.join(internal_dir, file))
-        # Log successful transfer
+        # Logging successful transfer message
         logging.info("Files transferred successfully")
 
 # Scheduling the script to run daily at a specific time
