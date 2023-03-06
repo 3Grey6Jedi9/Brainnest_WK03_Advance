72a442fd9629e9c27d99934e453470a63ac9f04c
Darth Vader <danieltarancon@gmail.com>
2023-03-06 16:54:53 +0100

    Update apps.py

--- apps.py
+++ apps.py
@@ -3,12 +3,13 @@ import ftplib
 import shutil
 import schedule
 import logging
-from pass import my_password
+
+
 
 # Define FTP server credentials
-ftp_host = "localhost"
-ftp_user = "admin"
-ftp_password = my_password
+ftp_host = "ftp.gnu.org"
+ftp_user = "anonymous"
+ftp_password = ""
 
 # Define local directory for storing downloaded files
 local_dir = "/Users/danielmulatarancon/Desktop/Documents/HACKING TIME/Brainnest /Week 03/Advance Tasks/Downloaded_files"
@@ -19,7 +20,7 @@ internal_dir = "/Users/danielmulatarancon/Desktop/Documents/HACKING TIME/Brainne
 # Define function to download files from FTP server
 def download_files():
     # Connect to FTP server
-    with ftplib.FTP_TLS(ftp_host, ftp_user, ftp_password) as ftp:
+    with ftplib.FTP(ftp_host, ftp_user, ftp_password) as ftp:
         # List files in FTP directory
         files = ftp.nlst()
         # Check if local directory exists, if not create it
@@ -27,8 +28,15 @@ def download_files():
             os.makedirs(local_dir)
         # Download each file to local directory
         for file in files:
-            with open(os.path.join(local_dir, file), "wb") as f:
-                ftp.retrbinary("RETR " + file, f.write)
+            local_file_path = os.path.join(local_dir, file)
+            try:
+                with open(local_file_path, "wb") as f:
+                    ftp.retrbinary("RETR " + file, f.write)
+                print(f"Successfully downloaded {file} to {local_file_path}")
+            except ftplib.error_perm as e:
+                print(f"Error downloading {file}: {e}")
+            except Exception as e:
+                print(f"Unexpected error downloading {file}: {e}")
         # Move files from local directory to internal network directory
         for file in os.listdir(local_dir):
             shutil.move(os.path.join(local_dir, file), os.path.join(internal_dir, file))
@@ -36,11 +44,13 @@ def download_files():
         logging.info("Files transferred successfully")
 
 # Schedule script to run daily at a specific time
-schedule.every().day.at("11:28").do(download_files)
+schedule.every().day.at("16:53").do(download_files)
 
 # Set up logging
 logging.basicConfig(filename="file_transfer.log", level=logging.INFO)
 
-# Start scheduled script
-while True:
-    schedule.run_pending()
+
+if __name__ == '__main__':
+    # Start scheduled script
+    while True:
+        schedule.run_pending()
